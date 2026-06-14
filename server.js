892f558f3cdc627eadc86e484a08a39dd8997c9a
João Santos <joao.santos@geofin.local>
2026-06-14 11:22:54 -0300

    Ajusta porta para deploy Render

--- server.js
+++ server.js
@@ -118,6 +118,8 @@ app.get("/historico", (req, res) => {
     res.json(lerHistorico());
 });
 
-app.listen(3000, () => {
-    console.log(" Servidor rodando em http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
